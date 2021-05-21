ac1dfebf8fd0d122a7a95834a6d3fb401ca62bec
DESKTOP-7JAGA93\pc24 <68523994+shfkd123@users.noreply.github.com>
2021-05-21 18:30:55 +0900

    Servlet
    Servlet

---
+++ command_bootstrap/src/kr/or/ddit/listener/ApplicationContextInitListener.java
@@ -0,0 +1,21 @@
+package kr.or.ddit.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class ApplicationContextInitListener implements ServletContextListener {
+
+    
+	public void contextInitialized(ServletContextEvent sce)  { 
+		String param = sce.getServletContext().getInitParameter("testParam");
+		System.out.println(param); 
+	}
+
+	public void contextDestroyed(ServletContextEvent arg0)  { 
+    	
+    }
+
+	
+}
--- command_bootstrap/test/kr/or/ddit/service/MockMenuDAO.java
+++ command_bootstrap/test/kr/or/ddit/service/MockMenuDAO.java
@@ -1,6 +1,7 @@
 package kr.or.ddit.service;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -16,6 +17,8 @@ public class MockMenuDAO implements MenuDAO {
 		
 		MenuVO menu = new MenuVO();
 		menu.setMname("회원가입");
+		
+		menuList = new ArrayList<MenuVO>();
 		menuList.add(menu);
 	
 		return menuList;
@@ -25,13 +28,15 @@ public class MockMenuDAO implements MenuDAO {
 	public List<MenuVO> selectSubMenu(SqlSession session, String mCode) throws SQLException {
 		List<MenuVO> menuList = null;
 		
+		if(session.getConnection()==null) throw new SQLException();
+		
 		MenuVO menu = new MenuVO();
 		menu.setMname("회원가입");
+		menuList = new ArrayList<MenuVO>();
 		menuList.add(menu);
 		
 		if(!mCode.equals("M010000")) menuList = null;
 	
-		
 		return menuList;
 	}
 
@@ -39,11 +44,12 @@ public class MockMenuDAO implements MenuDAO {
 	public MenuVO selectMenuByMcode(SqlSession session, String mCode) throws SQLException {
 		MenuVO menu = null;
 		
+		if(session.getConnection()==null) throw new SQLException();
+		
 		if(mCode.equals("M010200")) {
 			menu = new MenuVO();
 			menu.setMname("회원가입");
 		}
 		return menu;
 	}
-
 }
